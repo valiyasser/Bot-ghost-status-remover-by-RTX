90eea9317b50b16c3acc4aba54c734cce35a4eb0
valiyasser <165281333+valiyasser@users.noreply.github.com>
2024-03-28 15:22:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to SFR[","Watching SFRP"];
 
 
 let currentIndex = 0;
