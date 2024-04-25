6ac6467c9f00de76dd71c982d92f0aee62d7ed06
valiyt <168139255+valiyt@users.noreply.github.com>
2024-04-26 00:48:23 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["MODERATING (GU) GRINDERS UNITED!"];
+const statusMessages = ["MODERATING **GRINDERS UNITED**!"];
 
 
 let currentIndex = 0;
